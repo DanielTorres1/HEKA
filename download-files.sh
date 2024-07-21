57f5052295e1ea9d965ecd39beefdd6a7384f209
Daniel <daniel.torres@owasp.org>
2024-07-21 07:54:49 -0600

    update

--- download-files.sh
+++ download-files.sh
@@ -22,21 +22,63 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-echo "Comprimiendo"
-for i in {1..4}; do
-    ssh kali-"$nube-"$i "cd /root/HACKING && rm -f ${country}${i}.tar.gz && tar -zcvf ${country}${i}.tar.gz ${country}${i}"
-done
 
-echo "descargando"
-for i in {1..4}; do
-    scp kali-"$nube-"$i:/root/HACKING/${country}${i}.tar.gz .
-done
+if [ "$nube" = "amazon" ] || [ "$nube" = "digital" ]; then
 
-echo "descomprimiendo"
-for i in {1..4}; do
-    tar -zxvf ${country}${i}.tar.gz 
-    rm ${country}${i}.tar.gz 
-done
+    echo "Comprimiendo"
+    for i in {1..4}; do
+        index=$i
+        ssh kali-"$nube-"$index "cd /root/HACKING && rm -f ${country}${i}.tar.gz && tar -zcvf ${country}${i}.tar.gz ${country}${i}"
+    done
+
+    echo "descargando"
+    for i in {1..4}; do
+        index=$i
+        scp kali-"$nube-"$index:/root/HACKING/${country}${i}.tar.gz .
+    done
+
+    echo "descomprimiendo"
+    for i in {1..4}; do
+        tar -zxvf ${country}${i}.tar.gz 
+        rm ${country}${i}.tar.gz 
+    done
+fi
+
+
+
+
+#todas
+if [ "$nube" = "todas" ]; then
+    echo "Comprimiendo"
+    for i in {1..4}; do
+        index=$i
+        ssh kali-amazon-$index "cd /root/HACKING && rm -f ${country}${i}.tar.gz && tar -zcvf ${country}${i}.tar.gz ${country}${i}"
+    done
+    for i in {5..8}; do
+        index=$((i-4))
+        echo "ssh kali-digital-$index \"cd /root/HACKING && rm -f ${country}${i}.tar.gz && tar -zcvf ${country}${i}.tar.gz ${country}${i}\""
+        ssh kali-digital-$index "cd /root/HACKING && rm -f ${country}${i}.tar.gz && tar -zcvf ${country}${i}.tar.gz ${country}${i}"
+    done
+
+
+
+    echo "descargando"
+    for i in {1..4}; do
+        index=$i
+         scp kali-amazon-$index:/root/HACKING/${country}${i}.tar.gz .
+    done
+    for i in {5..8}; do
+        index=$((i-4))
+        scp kali-digital-$index:/root/HACKING/${country}${i}.tar.gz .
+    done
+
+
+    echo "descomprimiendo"
+    for i in {1..8}; do
+        tar -zxvf ${country}${i}.tar.gz 
+        rm ${country}${i}.tar.gz 
+    done
+fi
 
 echo "Uniendo logs y BDs"
 
